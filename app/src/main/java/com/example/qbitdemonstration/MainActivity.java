9910c7d6804b4edc4194ff60ddce83f3a4089962
joseph <joslunney@gmail.com>
2020-01-25 15:36:32 -0500

    Update November 13 2019

--- app/src/main/java/com/example/qbitdemonstration/MainActivity.java
+++ app/src/main/java/com/example/qbitdemonstration/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.Calendar;
+
 //hello
 public class MainActivity extends AppCompatActivity {
 
     private ListView doctorsList ;
     private dListAdapter DListAdapter ;
+    private Button dispenserButton ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         doctorsList = (ListView) findViewById(R.id.doctors_list) ;
+        dispenserButton = (Button) findViewById(R.id.dispenserButton) ;
 
         DListAdapter = new dListAdapter(this, getResources().getStringArray(R.array.dListItems)) ;
         doctorsList.setAdapter(DListAdapter);
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         doctorsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 5) {
+                if (position == 4) {
                     Intent intent = new Intent(getApplicationContext(), myDispenserActivity.class) ;
                     startActivity(intent);
                 }
@@ -35,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        dispenserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
--- app/src/main/java/com/example/qbitdemonstration/myDispenserActivity.java
+++ app/src/main/java/com/example/qbitdemonstration/myDispenserActivity.java
@@ -2,12 +2,18 @@ package com.example.qbitdemonstration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class myDispenserActivity extends AppCompatActivity {
+    TimePicker pillTimeSetter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_dispenser);
+
+        pillTimeSetter = (TimePicker) findViewById(R.id.pillTimePicker) ;
+
+
     }
 }
